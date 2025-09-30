419200059891a4424a14976aaefd9945dc1a3c3a
Mihaly Papp <mihaly@mihaly.codes>
2025-09-30 18:59:10 +0200

    fix main

--- main.go
+++ main.go
@@ -8,16 +8,10 @@ func main() {
 	log.Printf("hello world")
 
 	table := NewTable()
-	t := ReadUncommitted{
-		TransactionId: "1",
-		Table:         &table,
-	}
-	t.Set("key1", "value1").Commit()
+	t1 := NewReadUncommitted("1", &table)
+	t1.Set("key1", "value1").Commit()
 
-	t2 := ReadUncommitted{
-		TransactionId: "2",
-		Table:         &table,
-	}
+	t2 := NewReadUncommitted("2", &table)
 	t2.Set("key1", "value2")
 	t2.Delete("key1")
 	t2.Rollback()
